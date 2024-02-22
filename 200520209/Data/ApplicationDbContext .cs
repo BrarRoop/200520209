7c813ccb0983e4ef77c07e4ba1f744456a006fd0
BrarRoop <brarroopkaran@gmail.com>
2024-02-22 04:47:15 +0300

    Add project files.

---
+++ 200520209/Data/ApplicationDbContext .cs
@@ -0,0 +1,14 @@
+﻿using _200520209.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _200520209.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students{ get; set; }
+       
+    }
+}
---
+++ 200520209/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _200520209.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
